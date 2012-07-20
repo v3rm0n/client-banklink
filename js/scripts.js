492ce512e9b52ceb3bee0046b2c6f0417785dec4
Maido Käära <maidokaara@gmail.com>
2012-07-20 22:44:27 +0300

    Merchant request verification successful

--- js/scripts.js
+++ js/scripts.js
@@ -21,26 +21,24 @@ Packet.prototype.toMac = function(){
 		return str;
 	}
 	var macString = "";
-	for(param in this.parameters)
-		macString += pad(this.getParam(param).length)+this.getParam(param);
+	for(param in this.parameters){
+		if(param != "VK_MAC")
+			macString += pad(this.getParam(param).length)+this.getParam(param);
+	}
 	return macString;
 }
 //Sign parameters and return URL
 Packet.prototype.sign = function(){
-	$.get("rsa/merchant_cert.cer", function(data){
-		var rsa = new RSAKey();
-		rsa.readPrivateKeyFromPEMString(data);
-		var signature = rsa.signString(this.toMac(), "sha1");
-		setParam("VK_MAC", signature);
-	});
+	var rsa = new RSAKey();
+	rsa.readPrivateKeyFromPEMString(MERCHANT_PRIVATE_KEY);
+	var signature = rsa.signString(this.toMac(), "sha1");
+	this.setParam("VK_MAC", signature);
 }
 //Verifiy request parameters
 Packet.prototype.verify = function(){
-	$.get("rsa/merchant_cert.cer", function(data){
-		var x509 = new X509();
-		x509.readCertPEM(data);
-		return x509.subjectPublicKeyRSA.verifyString(this.toMac(), this.getParam("VK_MAC"));
-	});
+	var x509 = new X509();
+	x509.readCertPEM(MERCHANT_CERT);
+	return x509.subjectPublicKeyRSA.verifyString(this.toMac(), this.getParam("VK_MAC"));
 }
 Packet.prototype.html = function(){
 	var 	result = "<table class='table table-bordered table-striped'><thead><tr><th>Parameter</th><th>Value</th></tr></thead><tbody>";
@@ -72,3 +70,31 @@ Packet.init = function(){
 	}
 	return packet;
 }
+//Certs and stuff
+var MERCHANT_PRIVATE_KEY="-----BEGIN RSA PRIVATE KEY-----\
+MIICWwIBAAKBgQDRhGF7X4A0ZVlEg594WmODVVUIiiPQs04aLmvfg8SborHss5gQ\
+Xu0aIdUT6nb5rTh5hD2yfpF2WIW6M8z0WxRhwicgXwi80H1aLPf6lEPPLvN29EhQ\
+NjBpkFkAJUbS8uuhJEeKw0cE49g80eBBF4BCqSL6PFQbP9/rByxdxEoAIQIDAQAB\
+AoGAA9/q3Zk6ib2GFRpKDLO/O2KMnAfR+b4XJ6zMGeoZ7Lbpi3MW0Nawk9ckVaX0\
+ZVGqxbSIX5Cvp/yjHHpww+QbUFrw/gCjLiiYjM9E8C3uAF5AKJ0r4GBPl4u8K4bp\
+bXeSxSB60/wPQFiQAJVcA5xhZVzqNuF3EjuKdHsw+dk+dPECQQDubX/lVGFgD/xY\
+uchz56Yc7VHX+58BUkNSewSzwJRbcueqknXRWwj97SXqpnYfKqZq78dnEF10SWsr\
+/NMKi+7XAkEA4PVqDv/OZAbWr4syXZNv/Mpl4r5suzYMMUD9U8B2JIRnrhmGZPzL\
+x23N9J4hEJ+Xh8tSKVc80jOkrvGlSv+BxwJAaTOtjA3YTV+gU7Hdza53sCnSw/8F\
+YLrgc6NOJtYhX9xqdevbyn1lkU0zPr8mPYg/F84m6MXixm2iuSz8HZoyzwJARi2p\
+aYZ5/5B2lwroqnKdZBJMGKFpUDn7Mb5hiSgocxnvMkv6NjT66Xsi3iYakJII9q8C\
+Ma1qZvT/cigmdbAh7wJAQNXyoizuGEltiSaBXx4H29EdXNYWDJ9SS5f070BRbAIl\
+dqRh3rcNvpY6BKJqFapda1DjdcncZECMizT/GMrc1w==\
+-----END RSA PRIVATE KEY-----";
+var MERCHANT_CERT="-----BEGIN CERTIFICATE-----\
+MIIBvTCCASYCCQD55fNzc0WF7TANBgkqhkiG9w0BAQUFADAjMQswCQYDVQQGEwJK\
+UDEUMBIGA1UEChMLMDAtVEVTVC1SU0EwHhcNMTAwNTI4MDIwODUxWhcNMjAwNTI1\
+MDIwODUxWjAjMQswCQYDVQQGEwJKUDEUMBIGA1UEChMLMDAtVEVTVC1SU0EwgZ8w\
+DQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBANGEYXtfgDRlWUSDn3haY4NVVQiKI9Cz\
+Thoua9+DxJuiseyzmBBe7Roh1RPqdvmtOHmEPbJ+kXZYhbozzPRbFGHCJyBfCLzQ\
+fVos9/qUQ88u83b0SFA2MGmQWQAlRtLy66EkR4rDRwTj2DzR4EEXgEKpIvo8VBs/\
+3+sHLF3ESgAhAgMBAAEwDQYJKoZIhvcNAQEFBQADgYEAEZ6mXFFq3AzfaqWHmCy1\
+ARjlauYAa8ZmUFnLm0emg9dkVBJ63aEqARhtok6bDQDzSJxiLpCEF6G4b/Nv/M/M\
+LyhP+OoOTmETMegAVQMq71choVJyOFE5BtQa6M/lCHEOya5QUfoRF2HF9EjRF44K\
+3OK+u3ivTSj3zwjtpudY5Xo=\
+-----END CERTIFICATE-----";
